eb7edc4395f55ebcab60febcf1f56fb7c91450bf
Sebastian Garcia <sebas971129@gmail.com>
2018-08-17 15:59:27 -0500

    Avance 1

--- app/src/main/java/com/example/estudiante/ejercicios3/MainActivity.java
+++ app/src/main/java/com/example/estudiante/ejercicios3/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.estudiante.ejercicios3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_titulo;
+    EditText et_nombre;
+    Button btn_calcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_titulo = findViewById(R.id.tv_titulo);
+        et_nombre = findViewById(R.id.et_nombre);
+        btn_calcular = findViewById(R.id.btn_calcular);
+
+
+
+        btn_calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = et_nombre.getText().toString();
+                tv_titulo.setText("Hola "+nombre);
+
+                Toast.makeText(MainActivity.this, "Hola "+nombre, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
